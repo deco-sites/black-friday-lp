22090afa96454b71555fc3c8109a16a2a07f7c2d
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2023-09-25 18:29:06 -0300

    feat: MainBanner Image added

--- sections/Content/MainBanner.tsx
+++ sections/Content/MainBanner.tsx
@@ -8,6 +8,8 @@ export interface Props {
   content?: string;
   buttonText: string;
   buttonLink: string;
+  image: string;
+  alt?: string;
   /**
    * @format color
    */
@@ -18,20 +20,30 @@ export interface Props {
   textColor: string;
 }
 
-export default function MainBanner({backgroundColor, textColor, title, caption, content, buttonText, buttonLink}: Props) {
+export default function MainBanner({backgroundColor, textColor, title, caption, content, buttonText, buttonLink, image, alt}: Props) {
   return (
     <Layout backgroundColor={backgroundColor} textColor={textColor}>
-      <h2 className="text-base font-bold h-5 text-transparent bg-clip-text bg-gradient-to-b from-pink-500 to bg-purple-600">
-        {caption}
-      </h2>
-      <h1 className="text-5xl font-semibold">
-        {title}
-      </h1>
-      <p className="text-lg mt-[25px]">
-        {content}
-      </p>
-      <div>
-        <GradientButton text={buttonText} href={buttonLink} />
+      <div className="flex">
+        <div className="flex flex-col justify-center space-y-7">
+          <h2 className="text-base font-bold h-5 text-transparent bg-clip-text bg-gradient-to-b from-pink-500 to bg-purple-600">
+            {caption}
+          </h2>
+          <h1 className="text-5xl font-semibold">
+            {title}
+          </h1>
+          <p className="text-lg mt-[25px]">
+            {content}
+          </p>
+          <div>
+            <GradientButton text={buttonText} href={buttonLink} />
+          </div>
+        </div>
+        <Image
+          width={486} 
+          height={523}
+          src={image}
+          alt={alt}
+        />
       </div>
     </Layout>
   );
