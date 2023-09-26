3131ea0a44c92a1cffcffea348dc65e4828b5507
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2023-09-26 19:12:14 -0300

    feat: benefit image and alt props added

--- components/LandingPage/BenefitsLp.tsx
+++ components/LandingPage/BenefitsLp.tsx
@@ -1,10 +1,12 @@
 import type { ImageWidget as LiveImage } from "apps/admin/widgets.ts";
+import Image from "apps/website/components/Image.tsx";
 import Layout from "$store/components/LandingPage/Layout.tsx";
 
 export interface Benefit {
   icon: LiveImage;
   title: string;
   description: string;
+  alt?: string;
 }
 
 export interface Props {
@@ -26,6 +28,12 @@ export default function BenefitsLp ({benefits, backgroundColor, textColor}:Props
     <Layout backgroundColor={backgroundColor} textColor={textColor}>
       {benefits.map((b) => (
         <div>
+          <Image
+            width={54} 
+            height={54}
+            src={b.icon}
+            alt={b.alt}
+          />
           <h1>{b.title}</h1>
           <p>{b.description}</p>
         </div>
