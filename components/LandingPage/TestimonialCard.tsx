ac689708527f86bb576b6783945179eb36ba0cd3
EduardoBalbinot <eduardo.balbinot.dev@gmail.com>
2023-09-27 04:40:48 -0300

    style: testimonials customer pics are now rounded

--- components/LandingPage/TestimonialCard.tsx
+++ components/LandingPage/TestimonialCard.tsx
@@ -12,11 +12,13 @@ export default function({text, customerPic, customerName}: Props) {
     <div className="w-[80vw] sm:w-96 bg-white mx-3 min-h-[288px] my-9 rounded-2xl shadow-md p-8">
       <p className="min-h-[156px]">{text}</p>
       <div className="flex items-center gap-4">
-        <Image 
-          height={64}
-          width={64}
-          src={customerPic}
-        />
+        <div className="overflow-hidden rounded-full">
+          <Image 
+            height={64}
+            width={64}
+            src={customerPic}
+          />
+        </div>
         <label>{customerName}</label>
         </div>
       </div>
