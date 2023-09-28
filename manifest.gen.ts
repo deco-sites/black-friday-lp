d971d0b3ecb98365891b65db2bb774b3d81a5f5a
gabrielMatosBoubee <gm.boubee@gmail.com>
2023-09-28 10:24:53 -0300

    chore: metadados

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -3,32 +3,32 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $$$0 from "./loaders/Layouts/ProductCard.tsx";
-import * as $$$$$$0 from "./sections/Category/CategoryBanner.tsx";
-import * as $$$$$$1 from "./sections/Category/CategoryList.tsx";
-import * as $$$$$$2 from "./sections/Content/Benefits.tsx";
-import * as $$$$$$3 from "./sections/Content/Faq.tsx";
-import * as $$$$$$4 from "./sections/Content/Footer.tsx";
-import * as $$$$$$5 from "./sections/Content/Logos.tsx";
-import * as $$$$$$6 from "./sections/Content/MainBanner.tsx";
-import * as $$$$$$7 from "./sections/Content/Testimonials.tsx";
-import * as $$$$$$8 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$9 from "./sections/Header/Header.tsx";
-import * as $$$$$$10 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$11 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$12 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$13 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$14 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$15 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$16 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$17 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$18 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$19 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$20 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$21 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$22 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$23 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$24 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$25 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$0 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$1 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$2 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$3 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$4 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$5 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$6 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$7 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$8 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$9 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$10 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$11 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$12 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$13 from "./sections/Content/MainBanner.tsx";
+import * as $$$$$$14 from "./sections/Content/Faq.tsx";
+import * as $$$$$$15 from "./sections/Content/Footer.tsx";
+import * as $$$$$$16 from "./sections/Content/Benefits.tsx";
+import * as $$$$$$17 from "./sections/Content/Logos.tsx";
+import * as $$$$$$18 from "./sections/Content/Testimonials.tsx";
+import * as $$$$$$19 from "./sections/Header/Header.tsx";
+import * as $$$$$$20 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$21 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$22 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$23 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$24 from "./sections/Category/CategoryList.tsx";
+import * as $$$$$$25 from "./sections/Category/CategoryBanner.tsx";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
@@ -37,34 +37,34 @@ const manifest = {
     "deco-sites/black-friday-lp/loaders/Layouts/ProductCard.tsx": $$$0,
   },
   "sections": {
-    "deco-sites/black-friday-lp/sections/Category/CategoryBanner.tsx": $$$$$$0,
-    "deco-sites/black-friday-lp/sections/Category/CategoryList.tsx": $$$$$$1,
-    "deco-sites/black-friday-lp/sections/Content/Benefits.tsx": $$$$$$2,
-    "deco-sites/black-friday-lp/sections/Content/Faq.tsx": $$$$$$3,
-    "deco-sites/black-friday-lp/sections/Content/Footer.tsx": $$$$$$4,
-    "deco-sites/black-friday-lp/sections/Content/Logos.tsx": $$$$$$5,
-    "deco-sites/black-friday-lp/sections/Content/MainBanner.tsx": $$$$$$6,
-    "deco-sites/black-friday-lp/sections/Content/Testimonials.tsx": $$$$$$7,
-    "deco-sites/black-friday-lp/sections/Footer/Footer.tsx": $$$$$$8,
-    "deco-sites/black-friday-lp/sections/Header/Header.tsx": $$$$$$9,
-    "deco-sites/black-friday-lp/sections/Images/BannerGrid.tsx": $$$$$$10,
-    "deco-sites/black-friday-lp/sections/Images/Carousel.tsx": $$$$$$11,
-    "deco-sites/black-friday-lp/sections/Images/ImageGallery.tsx": $$$$$$12,
-    "deco-sites/black-friday-lp/sections/Images/ShoppableBanner.tsx": $$$$$$13,
-    "deco-sites/black-friday-lp/sections/Links/LinkTree.tsx": $$$$$$14,
-    "deco-sites/black-friday-lp/sections/Links/Shortcuts.tsx": $$$$$$15,
+    "deco-sites/black-friday-lp/sections/Category/CategoryBanner.tsx": $$$$$$25,
+    "deco-sites/black-friday-lp/sections/Category/CategoryList.tsx": $$$$$$24,
+    "deco-sites/black-friday-lp/sections/Content/Benefits.tsx": $$$$$$16,
+    "deco-sites/black-friday-lp/sections/Content/Faq.tsx": $$$$$$14,
+    "deco-sites/black-friday-lp/sections/Content/Footer.tsx": $$$$$$15,
+    "deco-sites/black-friday-lp/sections/Content/Logos.tsx": $$$$$$17,
+    "deco-sites/black-friday-lp/sections/Content/MainBanner.tsx": $$$$$$13,
+    "deco-sites/black-friday-lp/sections/Content/Testimonials.tsx": $$$$$$18,
+    "deco-sites/black-friday-lp/sections/Footer/Footer.tsx": $$$$$$0,
+    "deco-sites/black-friday-lp/sections/Header/Header.tsx": $$$$$$19,
+    "deco-sites/black-friday-lp/sections/Images/BannerGrid.tsx": $$$$$$23,
+    "deco-sites/black-friday-lp/sections/Images/Carousel.tsx": $$$$$$21,
+    "deco-sites/black-friday-lp/sections/Images/ImageGallery.tsx": $$$$$$22,
+    "deco-sites/black-friday-lp/sections/Images/ShoppableBanner.tsx": $$$$$$20,
+    "deco-sites/black-friday-lp/sections/Links/LinkTree.tsx": $$$$$$7,
+    "deco-sites/black-friday-lp/sections/Links/Shortcuts.tsx": $$$$$$8,
     "deco-sites/black-friday-lp/sections/Miscellaneous/CampaignTimer.tsx":
-      $$$$$$16,
+      $$$$$$2,
     "deco-sites/black-friday-lp/sections/Miscellaneous/CookieConsent.tsx":
-      $$$$$$17,
-    "deco-sites/black-friday-lp/sections/Newsletter/Newsletter.tsx": $$$$$$18,
-    "deco-sites/black-friday-lp/sections/Product/ProductDetails.tsx": $$$$$$19,
-    "deco-sites/black-friday-lp/sections/Product/ProductShelf.tsx": $$$$$$20,
-    "deco-sites/black-friday-lp/sections/Product/SearchResult.tsx": $$$$$$21,
-    "deco-sites/black-friday-lp/sections/Product/Wishlist.tsx": $$$$$$22,
-    "deco-sites/black-friday-lp/sections/Social/InstagramPosts.tsx": $$$$$$23,
-    "deco-sites/black-friday-lp/sections/Social/WhatsApp.tsx": $$$$$$24,
-    "deco-sites/black-friday-lp/sections/Theme/Theme.tsx": $$$$$$25,
+      $$$$$$1,
+    "deco-sites/black-friday-lp/sections/Newsletter/Newsletter.tsx": $$$$$$9,
+    "deco-sites/black-friday-lp/sections/Product/ProductDetails.tsx": $$$$$$4,
+    "deco-sites/black-friday-lp/sections/Product/ProductShelf.tsx": $$$$$$3,
+    "deco-sites/black-friday-lp/sections/Product/SearchResult.tsx": $$$$$$5,
+    "deco-sites/black-friday-lp/sections/Product/Wishlist.tsx": $$$$$$6,
+    "deco-sites/black-friday-lp/sections/Social/InstagramPosts.tsx": $$$$$$10,
+    "deco-sites/black-friday-lp/sections/Social/WhatsApp.tsx": $$$$$$11,
+    "deco-sites/black-friday-lp/sections/Theme/Theme.tsx": $$$$$$12,
   },
   "apps": {
     "deco-sites/black-friday-lp/apps/decohub.ts": $$$$$$$$$$$0,
